575e4f86ae79a47d58b23448c51029953b780703
debadutta <debadutta_singh@yahoo.co.in>
2025-06-19 12:59:33 +0530

    First Commit

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,20 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import base.BaseTest;
+import pages.LoginPage;
+
+public class LoginTest extends BaseTest {
+	@Test
+	public void testValidLogin() {
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.enterUsername("admin@yourstore.com");
+		loginPage.enterPassword("admin");
+		loginPage.clickLogin();
+		System.out.println("The title of the page is : "+driver.getTitle());
+	}
+	
+	
+
+}
